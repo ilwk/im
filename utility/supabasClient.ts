93382955120e29b61fbaac61242826191c3e745d
luwj <luwj@greatld.com>
2022-10-17 00:32:38 +0800

    chore: 增加supabase 类型提示

--- utility/supabasClient.ts
+++ utility/supabasClient.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
+import { Database } from 'db.types';
 
-export const supabase = createClient(
+export const supabase = createClient<Database>(
   String(process.env.NEXT_PUBLIC_SUPABASE_URL),
   String(process.env.NEXT_PUBLIC_SUPABASE_KEY)
 );
