4dbde9ca7f9504d147199673d3204557966c0bd1
Supabot <41898282+github-actions[bot]@users.noreply.github.com>
2022-10-17 06:22:50 +0000

    Update database types

--- db.types.ts
+++ db.types.ts
@@ -12,27 +12,47 @@ export interface Database {
       messages: {
         Row: {
           id: number;
+          created_at: string;
           username: string;
           room: string;
-          created_at: string;
           content: Json | null;
           type: string | null;
         };
         Insert: {
           id?: number;
+          created_at?: string;
           username: string;
           room: string;
-          created_at?: string;
           content?: Json | null;
           type?: string | null;
         };
         Update: {
           id?: number;
+          created_at?: string;
           username?: string;
           room?: string;
-          created_at?: string;
           content?: Json | null;
           type?: string | null;
+        };
+      };
+      rooms: {
+        Row: {
+          id: number;
+          created_at: string | null;
+          name: string;
+          status: string | null;
+        };
+        Insert: {
+          id?: number;
+          created_at?: string | null;
+          name: string;
+          status?: string | null;
+        };
+        Update: {
+          id?: number;
+          created_at?: string | null;
+          name?: string;
+          status?: string | null;
         };
       };
     };
